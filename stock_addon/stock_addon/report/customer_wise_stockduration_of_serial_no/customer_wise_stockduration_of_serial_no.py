3b447020193581257ecde41acbb4b15f7d6f8fb5
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-09-21 08:44:45 +0000

    fix: Date Range filter are now calculating No of Days in Stock respectivly

--- stock_addon/stock_addon/report/customer_wise_stockduration_of_serial_no/customer_wise_stockduration_of_serial_no.py
+++ stock_addon/stock_addon/report/customer_wise_stockduration_of_serial_no/customer_wise_stockduration_of_serial_no.py
@@ -22,13 +22,7 @@ def execute(filters=None):
 		else:
 			_filters = _filters + "where "
 		_filters = _filters + "customer = '" + filters.get("customer") + "'"
-	# else:
-	# 	columns.append({
-	# 		"fieldname":"customer",
-	# 		"label": frappe._("Customer"),
-	# 		"fieldtype": "Link",
-	# 		"options": "Customer"
-	# 	})
+
 	if filters.get("commission_by"):
 		if filters.get("commission_by") == frappe._("Item Wise"):
 			columns.append({
@@ -42,7 +36,7 @@ def execute(filters=None):
 				"fieldtype": "Link",
 				"options": "Project"
 			})
-		# 	pass
+
 		if filters.get("commission_by") == frappe._("Transaction Wise"):
 			columns.append({
 				"fieldname":"commission_t",
@@ -55,11 +49,6 @@ def execute(filters=None):
 				"fieldtype": "Link",
 				"options": "Project"
 			})
-			# if "where" in _filters:
-			# 	_filters = _filters + " and "
-			# else:
-			# 	_filters = _filters + "where "
-			# _filters = _filters + "project = '"+
 
 	data = []
 	_query = """
@@ -80,52 +69,60 @@ def execute(filters=None):
 		item_wise_i = None
 		item_wise_t = None
 		delivery_date = frappe.utils.getdate(sr.delivery_date if sr.delivery_date else frappe.utils.nowdate())
+
 		if filters.get("commission_by") == frappe._("Item Wise"):
 			if sr.purchase_document_type == "Purchase Receipt":
 				item_wise_i = frappe.db.get_value("Purchase Receipt",  sr.purchase_document_no, ["project", "kommission"],as_dict=1)
 			if sr.purchase_document_type == "Stock Entry":
 				item_wise_i = frappe.db.get_value("Stock Entry Detail", sr.purchase_document_no, ["project", "kommission"],as_dict=1)
+	
 		if filters.get("commission_by") == frappe._("Transaction Wise"):
 			if sr.purchase_document_type == "Purchase Receipt":
 				item_wise_t = frappe.db.get_value("Purchase Receipt Item", {"parent": sr.purchase_document_no}, ["project", "kommission"],as_dict=1)
 			if sr.purchase_document_type == "Stock Entry":
 				item_wise_t = frappe.db.get_value("Stock Entry Detail", {"parent":sr.purchase_document_no}, ["project", "kommission"],as_dict=1)
 
-		start = sr.delivery_date
-		end = sr.creation_date
+		start = sr.creation_date
+		end = sr.delivery_date
+
 		if filters.get("to_date") and filters.get("from_date"):
 			if sr.creation_date < frappe.utils.getdate(filters.get("from_date")):
-				end = frappe.utils.getdate(filters.get("from_date"))
-			elif sr.creation_date > frappe.utils.getdate(filters.get("from_date")):
-				end = sr.creation_date
-			else:
-				end = frappe.utils.getdate(filters.get("from_date"))
-			if sr.delivery_date and sr.delivery_date < frappe.utils.getdate(filters.get("to_date")):
-				start = sr.delivery_date
-			elif sr.delivery_date and sr.delivery_date > frappe.utils.getdate(filters.get("to_date")):
-				start = frappe.utils.getdate(filters.get("to_date"))
-			else:
-				start = frappe.utils.getdate(filters.get("to_date"))
-		diff = date_diff(start,end)+1
+				start = frappe.utils.getdate(filters.get("from_date"))
+				end = sr.delivery_date
+
+			if sr.creation_date > frappe.utils.getdate(filters.get("from_date")):
+				start = sr.creation_date
+				end = frappe.utils.getdate(filters.get("to_date"))
+
+			if sr.creation_date > frappe.utils.getdate(filters.get("from_date")) and delivery_date < frappe.utils.getdate(filters.get("to_date")):
+				start = sr.creation_date
+				end = sr.delivery_date
+
+		diff = date_diff(end, start)
 		if diff >= 0:
-			data.append(
-				{
-					"sr_id":sr.sr_id,
-					"creation_date": sr.creation_date,
-					"delivery_date": delivery_date,
-					"dis": diff,
-					"creation_doctype": sr.purchase_document_type,
-					"delivery_doctype": sr.delivery_document_type,
-					"creation_docname": sr.purchase_document_no,
-					"delivery_docname": sr.delivery_document_no,
-					"item_code": sr.item_code,
-					"customer": sr.customer,
-					"project_i": item_wise_i.project if item_wise_i else "-",
-					"commission_i": item_wise_i.kommission if item_wise_i else "-",
-					"project_t":  item_wise_t.project if item_wise_t else "-",
-					"commission_t": item_wise_t.kommission if item_wise_t else "-"
-				}
-			)
+			diff = diff+1
+
+		if diff <= 0:
+			diff = ((diff*-1)+1)
+
+		data.append(
+			{
+				"sr_id":sr.sr_id,
+				"creation_date": sr.creation_date,
+				"delivery_date": delivery_date,
+				"dis": diff,
+				"creation_doctype": sr.purchase_document_type,
+				"delivery_doctype": sr.delivery_document_type,
+				"creation_docname": sr.purchase_document_no,
+				"delivery_docname": sr.delivery_document_no,
+				"item_code": sr.item_code,
+				"customer": sr.customer,
+				"project_i": item_wise_i.project if item_wise_i else "-",
+				"commission_i": item_wise_i.kommission if item_wise_i else "-",
+				"project_t":  item_wise_t.project if item_wise_t else "-",
+				"commission_t": item_wise_t.kommission if item_wise_t else "-"
+			}
+		)
 
 	return columns, data
 
