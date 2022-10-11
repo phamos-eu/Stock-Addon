3bf884e62fd23869e57b8a3a21c69ecd6d2d9a38
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-10-11 06:55:50 +0000

    fix: considering 1 as storage factor and Cost per customer

--- stock_addon/stock_addon/report/customer_wise_stockduration_of_serial_no/customer_wise_stockduration_of_serial_no.py
+++ stock_addon/stock_addon/report/customer_wise_stockduration_of_serial_no/customer_wise_stockduration_of_serial_no.py
@@ -112,11 +112,13 @@ def execute(filters=None):
 
 		if diff >= 0:
 			diff = diff+1
-			storage_factor = frappe.db.get_value("Item", sr.item_code, 'lagerplatzfaktor') or 1
-			cost_per_customer = frappe.db.get_value("Customer", sr.customer, 'lagerplatzkosten') or 1
-			storage_cost = diff/storage_factor*cost_per_customer
-			if storage_factor == 1 or cost_per_customer == 1:
+			storage_factor = frappe.db.get_value("Item", sr.item_code, 'lagerplatzfaktor') or None
+			cost_per_customer = frappe.db.get_value("Customer", sr.customer, 'lagerplatzkosten') or None
+			if storage_factor == None or cost_per_customer == None:
 				storage_cost = 0
+			else:
+				storage_cost = diff/storage_factor*cost_per_customer
+				
 
 		if diff < 0:
 			continue
