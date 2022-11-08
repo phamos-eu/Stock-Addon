860b5708c15ef2be72afee7d75e2852ce7130cee
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-11-08 21:14:12 +0000

    fix: improvised Patch for create_accounting_dimension_for_kommission

--- stock_addon/patches/v0_1/create_accounting_dimension_for_kommission.py
+++ stock_addon/patches/v0_1/create_accounting_dimension_for_kommission.py
@@ -3,6 +3,8 @@ import frappe
 def execute():
 	frappe.reload_doc('accounts', 'doctype', 'accounting_dimension')
 	frappe.reload_doc("stock_addon", "doctype", "kommission")
+	if frappe.db.exists("Accounting Dimension", "Kommission"):
+		return
 	ad = frappe.new_doc("Accounting Dimension")
 	ad.document_type= "Kommission"
 	ad.label= "Kommission"
