03007edbf4a3369dfdc9973eb1be56e5ca54111c
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-11-21 07:21:03 +0000

    fix: split('\n') on NoneType Object

--- stock_addon/stock_addon/custom_hooks/stock_entry.py
+++ stock_addon/stock_addon/custom_hooks/stock_entry.py
@@ -4,6 +4,8 @@ import time
 def fetch_condition_from_stock_entry(self,method):
     time.sleep(5)
     for item in self.items:
-        list_serial = item.serial_no.split('\n')
+        list_serial = []
+        if item.serial_no:
+            list_serial = item.serial_no.split('\n')
         for ls in list_serial:
             frappe.db.set_value("Serial No", ls, "zustand",item.zustand)
