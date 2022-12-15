cc7e178f5a910c1b0df5d85b2872558d82895ea8
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-12-15 06:57:26 +0000

    fix: adjustable column width

--- stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.js
+++ stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.js
@@ -4,17 +4,24 @@
 frappe.query_reports["Lagerbestand pro Projekt und Zustand"] = {
 	"filters": [
 		{
-			"fieldname":"filter_date",
+			"fieldname": "filter_date",
 			"label": __("Datum (Stichtag)"),
 			"fieldtype": "Date",
 			"width": "60px"
 		},
 		{
-			"fieldname":"project_position",
+			"fieldname": "project",
 			"label": __("Project (Position)"),
 			"fieldtype": "Link",
 			"options": "Project",
 			"width": "60px"
+		},
+		{
+			"fieldname": "screen_size",
+			"label": "",
+			"fieldtype": "Data",
+			"hidden": 1,
+			"default": window.innerWidth
 		},
 	]
 };
--- stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
+++ stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
@@ -4,24 +4,27 @@
 import frappe
 
 def execute(filters=None):
-	columns = get_columns()
+	print("---------", filters)
+	columns = get_columns(filters.get('screen_size'))
 	data = []
 	where = "1=1"
-	if filters.get("project_position"):
-		where = f"{where} and project = '{filters.get('project_position')}'"
+	if filters.get("project"):
+		where = f"{where} and project = '{filters.get('project')}'"
 	if filters.get("filter_date"):
 		where = f"{where} and purchase_date <= '{filters.get('filter_date')}'"
-	serials = frappe.db.sql(f"select project, purchase_date, item_code, count(item_code) as item_code_count from `tabSerial No` where {where} and status='Active' group by item_code, project order by purchase_date desc" , as_dict=1)
-
+	query = f"select project, purchase_date, item_code, count(item_code) as item_code_count from `tabSerial No` where {where} and status='Active' group by item_code, project order by purchase_date desc"
+	serials = frappe.db.sql(query, as_dict=1)
 	for serial in serials:
-		new_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and project = '{serial.get('project')}' and zustand = 'Neu' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		used_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and project = '{serial.get('project')}' and zustand = 'Gebraucht' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		broken_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and project = '{serial.get('project')}' and zustand = 'Defekt' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		if serial.get("project"):
+			where = f"{where} and project = '{serial.get('project')}'"
+		new_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Neu' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		used_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Gebraucht' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		broken_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Defekt' group by item_code, project order by purchase_date desc" , as_dict=1) or []
 
 		data.append({
 			"item_code": serial.get('item_code'),
 			"actual_qty":serial.item_code_count,
-			"project_position":serial.get('project') or "", 
+			"project":serial.get('project') or "", 
 			"date":serial.get('purchase_date') or "", #Latest transaction
 			"new": new_[0].zustand if new_ else 0,
 			"used": used_[0].zustand if used_ else 0,
@@ -29,18 +32,19 @@ def execute(filters=None):
 		})
 
 	return columns, data
-def get_columns():
+def get_columns(screen_size):
 	columns = [
 		{
 			"label": frappe._("Artikel"),
 			"fieldname": "item_code",
 			"fieldtype": "Link",
 			"options": "Item",
-			"width": "120px"
+			"width": screen_size*0.3,
+			"align": "left"
 		},
 		{
-			"label": frappe._("Project Position"),
-			"fieldname": "project_position",
+			"label": frappe._("Project"),
+			"fieldname": "project",
 			"fieldtype": "Link",
 			"options": "Project"
 		},
@@ -57,17 +61,20 @@ def get_columns():
 		{
 			"label": frappe._("Neu"),
 			"fieldname": "new",
-			"fieldtype": "Number"
+			"fieldtype": "Number",
+			"width": screen_size*0.07,
 		},
 		{
 			"label": frappe._("Gebraucht"),
 			"fieldname": "used",
-			"fieldtype": "Number"
+			"fieldtype": "Number",
+			"width": screen_size*0.07,
 		},
 		{
 			"label": frappe._("Defekt"),
 			"fieldname": "broken",
-			"fieldtype": "Number"
+			"fieldtype": "Number",
+			"width": screen_size*0.07,
 		}
 	]
 	return columns
