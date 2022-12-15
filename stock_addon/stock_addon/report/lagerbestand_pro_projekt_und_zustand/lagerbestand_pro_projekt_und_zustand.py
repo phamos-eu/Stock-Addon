4a589e01eaab258bb540ea046731e2f5caa2cfd7
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-12-15 11:37:00 +0000

    chore: logic to fetch based on date

--- stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
+++ stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
@@ -4,7 +4,6 @@
 import frappe
 
 def execute(filters=None):
-	print("---------", filters)
 	columns = get_columns(filters.get('screen_size'))
 	data = []
 	where = "1=1"
@@ -12,14 +11,16 @@ def execute(filters=None):
 		where = f"{where} and project = '{filters.get('project')}'"
 	if filters.get("filter_date"):
 		where = f"{where} and purchase_date <= '{filters.get('filter_date')}'"
-	query = f"select project, purchase_date, item_code, count(item_code) as item_code_count from `tabSerial No` where {where} and status='Active' group by item_code, project order by purchase_date desc"
+	else:
+		where = f"{where} and status='Active'"
+	query = f"select project, purchase_date, item_code, count(item_code) as item_code_count from `tabSerial No` where {where} group by item_code, project order by purchase_date desc"
 	serials = frappe.db.sql(query, as_dict=1)
 	for serial in serials:
 		if serial.get("project"):
 			where = f"{where} and project = '{serial.get('project')}'"
-		new_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Neu' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		used_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Gebraucht' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		broken_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and status='Active' and item_code = '{serial.get('item_code')}' and zustand = 'Defekt' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		new_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Neu' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		used_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Gebraucht' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		broken_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Defekt' group by item_code, project order by purchase_date desc" , as_dict=1) or []
 
 		data.append({
 			"item_code": serial.get('item_code'),
