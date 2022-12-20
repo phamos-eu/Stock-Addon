d6b79cd4bbf36a4b8b067b659d2aeef4dc60cba2
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-12-20 17:07:00 +0000

    fix: on create of stock entry fetch Project to Serial No

--- stock_addon/stock_addon/custom_hooks/stock_entry.py
+++ stock_addon/stock_addon/custom_hooks/stock_entry.py
@@ -8,4 +8,7 @@ def fetch_condition_from_stock_entry(self,method):
         if item.serial_no:
             list_serial = item.serial_no.split('\n')
         for ls in list_serial:
-            frappe.db.set_value("Serial No", ls, "zustand",item.zustand)
+            add_value = {"zustand":item.zustand}
+            if self.project:
+                add_value.update({"project":self.project})
+            frappe.db.set_value("Serial No", ls, add_value)
