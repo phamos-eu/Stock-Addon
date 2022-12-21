e57858341e22aee0efbd4241eadccacf5c22825c
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2022-12-21 09:16:36 +0000

    fix: report showing wrong qty

--- stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
+++ stock_addon/stock_addon/report/lagerbestand_pro_projekt_und_zustand/lagerbestand_pro_projekt_und_zustand.py
@@ -6,32 +6,41 @@ import frappe
 def execute(filters=None):
 	columns = get_columns(filters.get('screen_size'))
 	data = []
-	where = "1=1"
+	where = ""
+	where_project = ""
+	zustand_where = ""
+	# where_modified = ""
 	if filters.get("project"):
-		where = f"{where} and project = '{filters.get('project')}'"
+		where_project = f" where name= '{filters.get('project')}'"
 	if filters.get("filter_date"):
-		where = f"{where} and purchase_date <= '{filters.get('filter_date')}'"
-	else:
-		where = f"{where} and status='Active'"
-	query = f"select project, purchase_date, item_code, count(item_code) as item_code_count from `tabSerial No` where {where} group by item_code, project order by purchase_date desc"
-	serials = frappe.db.sql(query, as_dict=1)
-	for serial in serials:
-		if serial.get("project"):
-			where = f"{where} and project = '{serial.get('project')}'"
-		new_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Neu' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		used_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Gebraucht' group by item_code, project order by purchase_date desc" , as_dict=1) or []
-		broken_ = frappe.db.sql(f"select count(zustand) as zustand from `tabSerial No` where {where} and item_code = '{serial.get('item_code')}' and zustand = 'Defekt' group by item_code, project order by purchase_date desc" , as_dict=1) or []
+		where = f"and posting_date <= '{filters.get('filter_date')}'"
+		zustand_where = f"and purchase_date <= '{filters.get('filter_date')}'"
 
-		data.append({
-			"item_code": serial.get('item_code'),
-			"actual_qty":serial.item_code_count,
-			"project":serial.get('project') or "", 
-			"date":serial.get('purchase_date') or "", #Latest transaction
-			"new": new_[0].zustand if new_ else 0,
-			"used": used_[0].zustand if used_ else 0,
-			"broken": broken_[0].zustand if broken_ else 0
-		})
+	project_list = [v.name for v in frappe.db.sql(f"""select name from `tabProject`{where_project}""",as_dict=1)]
+	project_list.append("")
+	item_list = frappe.get_all("Item", filters={'has_serial_no':1},pluck='name')
 
+	for project in project_list:
+		if project == "":
+			project_conditions = f"and project is NULL"
+		else:
+			project_conditions = f"and project='{project}'"
+		for item in item_list:
+			qty_date = frappe.db.sql(f"""select qty_after_transaction as qty, posting_date from `tabStock Ledger Entry` where item_code='{item}'{project_conditions} {where} order by posting_date desc LIMIT 1""",as_dict=1)
+			print("===",f"""select qty_after_transaction as qty, posting_date from `tabStock Ledger Entry` where item_code='{item}'{project_conditions} {where} order by posting_date desc LIMIT 1""")
+			new_serial = frappe.db.sql(f"""select count(zustand='Neu') as qty from `tabSerial No` where item_code='{item}'{project_conditions} and zustand='Neu' {zustand_where}""",as_dict=1)
+			used_serial = frappe.db.sql(f"""select count(zustand='Gebrauch') as qty from `tabSerial No` where item_code='{item}'{project_conditions} and zustand='Gebrauch' {zustand_where}""",as_dict=1)
+			defected_serial = frappe.db.sql(f"""select count(zustand='Defekt') as qty from `tabSerial No` where item_code='{item}'{project_conditions} and zustand='Defekt' {zustand_where}""",as_dict=1)
+			if qty_date:
+				data.append({
+					"item_code": item,
+					"project": project,
+					"actual_qty":qty_date[0].get('qty'),
+					"date": qty_date[0].get('posting_date'),
+					"new": new_serial[0].get('qty'),
+					"used": used_serial[0].get('qty'),
+					"broken": defected_serial[0].get('qty')
+				})
 	return columns, data
 def get_columns(screen_size):
 	columns = [
@@ -57,7 +66,8 @@ def get_columns(screen_size):
 		{
 			"label": frappe._("Datum"),
 			"fieldname": "date",
-			"fieldtype": "Date"
+			"fieldtype": "Date",
+			"width": screen_size*0.07
 		},
 		{
 			"label": frappe._("Neu"),
